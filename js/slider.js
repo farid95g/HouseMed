e31de1dc276014e2925a6cd4a9a5a3f52b2e0b1e
farid95g <f95guluzade@gmail.com>
2021-07-11 18:18:33 +0400

    change slider item on button clicks

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,49 @@
 // SLIDER VARIABLES
 const sliderItems = document.querySelectorAll('.slider__items--item');
+const sliderButtons = document.querySelectorAll('.slider__btn button');
 
 // LOAD ANIMATION FOR SLIDER ITEM
 document.addEventListener('DOMContentLoaded', () => {
   sliderItems[0].classList.add('show');
   const sliderChildren = sliderItems[0].children[0];
 
-  setTimeout(() => {
-    sliderChildren.children[0].classList.add('visible');
-  }, 100);
-  setTimeout(() => {
-    sliderChildren.children[1].classList.add('visible');
-  }, 200);
-  setTimeout(() => {
-    sliderChildren.children[2].classList.add('visible');
-  }, 300);
-  setTimeout(() => {
-    sliderChildren.children[3].classList.add('visible');
-  }, 500);
+  animateSliderItem(null, sliderChildren.children);
+});
+
+// SLIDER ITEM ELEMENTS ANIMATION FUNCTION
+function animateSliderItem(prevItems, currItems) {
+  if (prevItems) {
+    [...prevItems].forEach(item => {
+      item.classList.remove('visible');
+    });
+  }
+  
+  [...currItems].forEach((item, i) => {
+    setTimeout(() => {
+      item.classList.add('visible');
+    }, (i + 1) * 100);
+  });
+}
+
+// CHANGE SLIDER ITEM ON BUTTON CLICKS
+sliderButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const currentIndex = [...sliderItems].indexOf(document.querySelector('.slider__items--item.show'));
+
+    if (button.parentElement.classList.value.includes('right')) {
+      sliderItems[currentIndex].classList.remove('show');
+      sliderItems[currentIndex + 1].classList.add('show');
+
+      const sliderChildren = sliderItems[currentIndex + 1].children[0];
+
+      animateSliderItem(sliderItems[currentIndex].children[0].children, sliderChildren.children);
+    } else {
+      sliderItems[currentIndex].classList.remove('show');
+      sliderItems[currentIndex - 1].classList.add('show');
+
+      const sliderChildren = sliderItems[currentIndex - 1].children[0];
+
+      animateSliderItem(sliderItems[currentIndex].children[0].children, sliderChildren.children);
+    }
+  })
 })
