56cf3f1b2124a1040ad6264d4be153435f875e10
farid95g <f95guluzade@gmail.com>
2021-07-30 00:37:49 +0400

    header navigation links position changes

--- js/header.js
+++ js/header.js
@@ -3,12 +3,12 @@ const active = document.querySelector(".navbar__left--active");
 const width = active.clientWidth;
 const left = active.offsetLeft;
 
-links.forEach(link => {
+links.forEach((link, i) => {
   link.addEventListener("mouseenter", () => {
     const span = link.querySelector("span");
     const style = `
       width: ${span.clientWidth + 14}px;
-      left: ${link.offsetLeft + ((link.clientWidth - (span.clientWidth + 14)) / 2)}px;
+      left: ${link.offsetLeft + ((link.clientWidth - (span.clientWidth + (i !== links.length - 1 ? 14 : 7))) / 2)}px;
     `;
     active.setAttribute("style", style);
   });
